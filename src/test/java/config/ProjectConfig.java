2eea21e10cc3c861c0b19854a8605e5871d54977
Artem Belsky <belsartem@gmail.com>
2024-03-31 19:44:04 +0200

    Правка по замечанию по энвам

--- src/test/java/config/ProjectConfig.java
+++ src/test/java/config/ProjectConfig.java
@@ -2,7 +2,7 @@ package config;
 
 import org.aeonbits.owner.Config;
 
-@Config.Sources("classpath:config/${environment}.properties")
+@Config.Sources("classpath:config/${env}.properties")
 
 public interface ProjectConfig extends Config {
 
--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -4,22 +4,38 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import config.DriverConfig;
+import config.ProjectConfig;
 import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.Config;
 import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.Map;
 
+@Config.LoadPolicy(Config.LoadType.FIRST)
+@Config.Sources({
+        "classpath:config/${env}.properties",
+        "classpath:config/stage.properties"
+})
 public class BaseTest {
+
+/*    private ProjectConfig projectConfig;
+
+    @BeforeEach
+    void setTestData() {
+        System.setProperty("env", System.getProperty("env", "test"));
+        projectConfig = ConfigFactory.create(ProjectConfig.class);
+    }*/
 
     @BeforeAll
     static void beforeAll() {
 
         DriverConfig driverConfig = ConfigFactory.create(DriverConfig.class);
-        System.setProperty("environment", System.getProperty("environment", "stage"));
+        //System.setProperty("environment", System.getProperty("environment", "stage"));
 
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
